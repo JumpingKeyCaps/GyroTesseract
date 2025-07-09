7149ba30a9936572f277ec36ccfe9603ef8583ee
Samuel Lebailly <sam.meusa@gmail.com>
2025-07-09 18:01:37 +0200

    feature(model): Add the vertex data model.

---
+++ app/src/main/java/com/lebaillyapp/gyrotesseract/model/Vertex.kt
@@ -0,0 +1,10 @@
+package com.lebaillyapp.gyrotesseract.model
+
+/**
+ * Représente un point dans l'espace 3D.
+ *
+ * @property x Coordonnée X
+ * @property y Coordonnée Y
+ * @property z Coordonnée Z
+ */
+data class Vertex(val x: Float, val y: Float, val z: Float)
